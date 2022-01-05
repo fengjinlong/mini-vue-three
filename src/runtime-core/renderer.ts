23078462c16d8bbfeaa427f661cd7b21fe07de0e
fjl <18618166564@163.com>
2022-01-05 14:55:35 +0800

    move 0

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,4 +1,5 @@
 import { isObject } from "../shared/index";
+import { ShapeFlags } from "../shared/ShapeFlags";
 import { createComponentInstance, setupComponent } from "./components";
 
 export function render(vnode, container) {
@@ -7,9 +8,11 @@ export function render(vnode, container) {
 }
 
 function patch(vnode: any, container: any) {
-  if (typeof vnode.type === "string") {
+  const { shapeFlag } = vnode;
+
+  if (shapeFlag & ShapeFlags.ELEMENT) {
     processElement(vnode, container);
-  } else if (isObject(vnode.type)) {
+  } else if (shapeFlag & ShapeFlags.STATEFUL_COMPONENT) {
     processComponent(vnode, container);
   }
 }
@@ -22,10 +25,10 @@ function mountElement(vnode: any, container: any) {
   const el = (vnode.el = document.createElement(vnode.type));
 
   // children 有 string array
-  const { children, props } = vnode;
-  if (typeof children === "string") {
+  const { children, props, shapeFlag } = vnode;
+  if (shapeFlag & ShapeFlags.TEXT_CHILDREN) {
     el.textContent = children;
-  } else if (Array.isArray(children)) {
+  } else if (shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
     mountChildren(children, el);
   }
   for (const key in props) {
--- src/runtime-core/vnode.ts
+++ src/runtime-core/vnode.ts
@@ -1,9 +1,23 @@
+import { ShapeFlags } from "../shared/ShapeFlags";
+
 export function createVNode(type, props?, children?) {
-  const vndoe = {
+  const vnode = {
     type,
     props,
     children,
-    el:null
+    shapeFlag: getShapeFlag(type),
+    el: null,
   };
-  return vndoe;
+  // children
+  if (typeof children === "string") {
+    vnode.shapeFlag |= ShapeFlags.TEXT_CHILDREN
+  } else if (Array.isArray(children)) {
+    vnode.shapeFlag |= ShapeFlags.ARRAY_CHILDREN
+  }
+  return vnode;
+}
+function getShapeFlag(type) {
+  return typeof type === "string"
+    ? ShapeFlags.ELEMENT
+    : ShapeFlags.STATEFUL_COMPONENT;
 }
