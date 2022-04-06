0c69324eaebad1f5274ed20d7fcf9411ce365b96
fjl <18618166564@163.com>
2022-04-06 22:02:06 +0800

    msg

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -17,9 +17,13 @@ function parseChildren(context): any {
 
   if (s.startsWith("{{")) {
     node = parseInterpolation(context);
+  } else if (s[0] === "<") {
+    if (/[a-z]/i.test(s[1])) {
+      node = parseElement(context);
+    }
   }
   nodes.push(node);
-  return nodes
+  return nodes;
 }
 
 function createRoot(children) {
@@ -36,15 +40,15 @@ function parseInterpolation(context: any): any {
     closeDelimiter,
     openDelimiter.length
   );
-  
+
   advanceBy(context, openDelimiter.length);
-  
+
   const rawContentLength = closeIndex - openDelimiter.length;
-  
+
   // const rawContent = parseTextData(context, rawContentLength);
   const rawContent = context.source.slice(0, rawContentLength);
   const content = rawContent.trim();
-  
+
   advanceBy(context, closeDelimiter.length);
 
   return {
@@ -57,4 +61,25 @@ function parseInterpolation(context: any): any {
 }
 function advanceBy(context: any, length: number) {
   context.source = context.source.slice(length);
+}
+const enum TagType {
+  Start,
+  End,
+}
+function parseElement(context: any) {
+  const element = parseTag(context, TagType.Start);
+  parseTag(context, TagType.End);
+  console.log("---" + context.source);
+  return element;
+}
+function parseTag(context: any, type: TagType) {
+  const match: any = /^<\/?([a-z]*)/i.exec(context.source);
+  const tag = match[1];
+  advanceBy(context, match[0].length);
+  advanceBy(context, 1);
+  if (type === TagType.End) return;
+  return {
+    type: NodeTypes.ELEMENT,
+    tag,
+  };
 }
--- src/compiler-core/tests/parse.spec.ts
+++ src/compiler-core/tests/parse.spec.ts
@@ -8,9 +8,20 @@ describe("Parse", () => {
       expect(ast.children[0]).toStrictEqual({
         type: NodeTypes.INTERPOLATION,
         content: {
-          type:NodeTypes.SIMPLE_EXPRESSION,
-          content: "message"
-        }
+          type: NodeTypes.SIMPLE_EXPRESSION,
+          content: "message",
+        },
+      });
+    });
+  });
+  describe("element", () => {
+    it("simple element div", () => {
+      const ast = baseParse("<div></div>");
+
+      expect(ast.children[0]).toStrictEqual({
+        type: NodeTypes.ELEMENT,
+        tag: "div",
+        // children: [],
       });
     });
   });
