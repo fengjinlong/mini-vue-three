ca9086424469657c329ae691fd7f0068018a8bf5
fjl <18618166564@163.com>
2022-04-10 23:15:51 +0800

    bug

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -2,7 +2,7 @@ import { NodeTypes } from "./ast";
 
 export function baseParse(content: string) {
   const context = createParserContext(content);
-  return createRoot(parseChildren(context));
+  return createRoot(parseChildren(context, ""));
 }
 
 function createParserContext(content: string) {
@@ -10,20 +10,32 @@ function createParserContext(content: string) {
     source: content,
   };
 }
-function parseChildren(context): any {
+function parseChildren(context, parentTag): any {
   const nodes: any = [];
-  let node;
-  let s = context.source;
+  while (!isEnd(context, parentTag)) {
+    let node;
+    let s = context.source;
 
-  if (s.startsWith("{{")) {
-    node = parseInterpolation(context);
-  } else if (s[0] === "<") {
-    if (/[a-z]/i.test(s[1])) {
-      node = parseElement(context);
+    if (s.startsWith("{{")) {
+      node = parseInterpolation(context);
+    } else if (s[0] === "<") {
+      if (/[a-z]/i.test(s[1])) {
+        node = parseElement(context);
+      }
     }
+    if (!node) {
+      node = parseText(context);
+    }
+    nodes.push(node);
   }
-  nodes.push(node);
   return nodes;
+}
+function isEnd(context, parentTag) {
+  const s = context.source;
+  if (parentTag && s.startsWith(`</${parentTag}>`)) {
+    return true;
+  }
+  return !context.source;
 }
 
 function createRoot(children) {
@@ -31,6 +43,7 @@ function createRoot(children) {
     children,
   };
 }
+
 
 function parseInterpolation(context: any): any {
   // {{message}}
@@ -46,7 +59,7 @@ function parseInterpolation(context: any): any {
   const rawContentLength = closeIndex - openDelimiter.length;
 
   // const rawContent = parseTextData(context, rawContentLength);
-  const rawContent = context.source.slice(0, rawContentLength);
+  const rawContent = parseTextData(context, rawContentLength);
   const content = rawContent.trim();
 
   advanceBy(context, closeDelimiter.length);
@@ -67,9 +80,9 @@ const enum TagType {
   End,
 }
 function parseElement(context: any) {
-  const element = parseTag(context, TagType.Start);
+  const element: any = parseTag(context, TagType.Start);
   parseTag(context, TagType.End);
-  console.log("---" + context.source);
+  element.children = parseChildren(context, element.tag);
   return element;
 }
 function parseTag(context: any, type: TagType) {
@@ -82,4 +95,42 @@ function parseTag(context: any, type: TagType) {
     type: NodeTypes.ELEMENT,
     tag,
   };
+}
+function parseText1(context: any): any {
+  let endIndex = context.source.length;
+  let endTokens = ["<", "{{"];
+  for (let i = 0; i < endTokens.length; i++) {
+    const index = context.source.indexOf(endTokens[i]);
+    if (index !== -1 && endIndex > index) {
+      endIndex = index;
+    }
+  }
+  const content = parseTextData(context, endIndex);
+  return {
+    type: NodeTypes.TEXT,
+    content,
+  };
+}
+function parseText(context: any) {
+  let endIndex = context.source.length;
+  let endTokens = ["</", "{{"];
+
+  for (let i = 0; i < endTokens.length; i++) {
+    const index = context.source.indexOf(endTokens[i]);
+    if (index !== -1 && endIndex > index) {
+      endIndex = index;
+    }
+  }
+
+  const content = parseTextData(context, endIndex);
+
+  return {
+    type: NodeTypes.TEXT,
+    content,
+  };
+}
+function parseTextData(context: any, length) {
+  const content = context.source.slice(0, length);
+  advanceBy(context, length);
+  return content;
 }
